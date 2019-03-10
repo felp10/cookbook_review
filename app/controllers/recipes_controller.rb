5f1d90a24188f3fe20d703861aa2229fba19201f
Felipe Terron <felipe_terron@yahoo.com.br>
2019-03-10 14:41:08 -0300

    Erro na action create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,23 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+                                   :cook_time, :ingredients, :cook_method)
+  end
 end
