5df6390381a162df33ed3f291d989bdb21ebe3d6
Felipe Terron <felipe_terron@yahoo.com.br>
2019-03-11 12:04:36 -0300

    Feature User edit recipe

--- spec/features/user_edit_recipe_spec.rb
+++ spec/features/user_edit_recipe_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 feature 'User update recipe' do
   scenario 'successfully' do
-    Recipe.create(title: 'Bolodecenoura', difficulty: 'Médio',
+    Recipe.create(title: 'Bolo de cenoura', difficulty: 'Médio',
                   cook_time: 50,
                   ingredients: 'Farinha, açucar, cenoura',
                   cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
 
     # simula a ação do usuário
     visit root_path
-    click_on 'Bolodecenoura'
+    click_on 'Bolo de cenoura'
     click_on 'Editar'
 
     fill_in 'Título', with: 'Bolo de cenoura'
