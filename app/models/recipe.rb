cd6a55672a1610ee8a568b9349d82073d9061557
Felipe Terron <felipe_terron@yahoo.com.br>
2019-03-11 16:19:22 -0300

    Feature User edit recipe - Validate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,14 @@
 class Recipe < ApplicationRecord
+  validates :title, presence: true
+  validates :recipe_type, presence: true
+  validates :difficulty, presence: true
+  validates :cuisine, presence: true
+  validates :cook_time, presence: true
+  validates :cook_time, numericality: { other_than: 0, message: 'Tempo de preparo tem que ser maior que zero!' }
+  validates :cook_time, numericality: { greater_than: 0, message: 'Tempo de preparo não pode ser negativo' }
+  validates :ingredients, presence: true
+  validates :cook_method, presence: true
+
   def cook_time_min
     "#{cook_time} minutos."
   end
